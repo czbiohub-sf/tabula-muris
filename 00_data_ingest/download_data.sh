82897dfc15ec59fec70be4f99be6feb0900f1d6b
Joshua Batson <joshua.batson@czbiohub.org>
2018-01-12 13:19:19 -0800

    download script now unzips

--- 00_data_ingest/download_data.sh
+++ 00_data_ingest/download_data.sh
@@ -6,3 +6,10 @@ echo "Downloading FACS data zip file to $DIR"
 curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/facs_raw_data.zip
 echo "Downloading Droplet data zip file to $DIR"
 curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/droplet_raw_data.zip
+
+echo "Unzipping directories"
+
+unzip droplet_raw_data.zip -d droplet_raw_data
+unzip facs_raw_data.zip -d facs_raw_data
+unzip facs_raw_data/FACS.zip -d facs_raw_data/
+unzip droplet_raw_data/droplet.zip -d droplet_raw_data/
