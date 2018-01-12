e8227730226e1d1d886b0ce0d9f0702055b976c4
Joshua Batson <joshua.batson@czbiohub.org>
2018-01-12 10:02:21 -0800

    added 'raw' to zip file names to match the documentation

--- 00_data_ingest/download_data.sh
+++ 00_data_ingest/download_data.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Downloading FACS data zip file to $DIR"
-curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/facs_data.zip
+curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/facs_raw_data.zip
 echo "Downloading Droplet data zip file to $DIR"
-curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/droplet_data.zip
+curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/droplet_raw_data.zip
