5b51759aa517b1f0b3688de516c8f20c2bc8222d
olgabot <olga.botvinnik@gmail.com>
2018-01-22 17:05:50 -0800

    Restructure data ingest folder

--- 00_data_ingest/download_data.sh
+++ 00_data_ingest/download_data.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Downloading FACS data zip file to $DIR"
-curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/facs_raw_data.zip
+curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/00_facs_raw_data.zip
 echo "Downloading Droplet data zip file to $DIR"
-curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/droplet_raw_data.zip
+curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/00_droplet_raw_data.zip
 
 echo "Unzipping directories"
 
