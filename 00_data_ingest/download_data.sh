678a6e118ac75b954a8addb5181637d283a4626c
Joshua Batson <joshua.batson@czbiohub.org>
2018-02-02 13:41:21 -0800

    Updaded download script to include 00/01 directory prefixes

--- 00_data_ingest/download_data.sh
+++ 00_data_ingest/download_data.sh
@@ -5,11 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Downloading FACS data zip file to $DIR"
 curl https://ndownloader.figshare.com/articles/5715040/versions/1 > $DIR/00_facs_raw_data.zip
 echo "Downloading Droplet data zip file to $DIR"
-curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/00_droplet_raw_data.zip
+curl https://ndownloader.figshare.com/articles/5715025/versions/1 > $DIR/01_droplet_raw_data.zip
 
 echo "Unzipping directories"
 
-unzip droplet_raw_data.zip -d droplet_raw_data
-unzip facs_raw_data.zip -d facs_raw_data
-unzip facs_raw_data/FACS.zip -d facs_raw_data/
-unzip droplet_raw_data/droplet.zip -d droplet_raw_data/
+unzip 00_facs_raw_data.zip -d 00_facs_raw_data
+unzip 01_droplet_raw_data.zip -d 01_droplet_raw_data
+unzip 00_facs_raw_data/FACS.zip -d 00_facs_raw_data/
+unzip 01_droplet_raw_data/droplet.zip -d 01_droplet_raw_data/
+
+rm 00_facs_raw_data.zip
+rm 01_droplet_raw_data.zip
