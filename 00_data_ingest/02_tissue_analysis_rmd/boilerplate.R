4cf8ffddee05a7970471b10b9434860592da420c
Joshua Batson <joshua.batson@czbiohub.org>
2018-02-26 16:15:28 -0800

    added mouse.id to droplet boilerplate

--- 00_data_ingest/02_tissue_analysis_rmd/boilerplate.R
+++ 00_data_ingest/02_tissue_analysis_rmd/boilerplate.R
@@ -113,7 +113,7 @@ load_tissue_droplet = function(tissue_of_interest){
   droplet_metadata <- read.csv(droplet_metadata_filename, sep=",", header = TRUE)
   colnames(droplet_metadata)[1] <- "channel"
 
-  tissue_metadata = filter(droplet_metadata, tissue == tissue_of_interest)[,c('channel','tissue','subtissue','mouse.sex')]
+  tissue_metadata = filter(droplet_metadata, tissue == tissue_of_interest)[,c('channel','tissue','subtissue','mouse.sex', 'mouse.id')]
   
   subfolder = paste0(tissue_of_interest, '-', tissue_metadata$channel[1])
   raw.data <- Read10X(data.dir = here('00_data_ingest', '01_droplet_raw_data', 'droplet', subfolder))
