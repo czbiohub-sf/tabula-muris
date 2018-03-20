cb45bd45c7df3186fbbc2a2153ec68db727ec57e
olgabot <olga.botvinnik@gmail.com>
2018-03-19 17:40:53 -0700

    Separate out commonly used code into functions (!!)

---
+++ 30_tissue_supplement_figures/supplemental_figures.R
@@ -0,0 +1,34 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: olgabot
+# Created on: 3/19/18
+
+# Commonly used variables by all Rmd files
+save_folder = here('30_tissue_supplement_figures', '{TISSUE}', '{METHOD}')
+standard.group.bys = c("cell_ontology_class", "free_annotation")
+
+
+# Make all supplemental figures for a tiss object
+dot_tsne_violin = function(tiss, genes_to_check, save_folder, prefix, group.bys){
+    for (group.by in group.bys ){
+
+      filename = file.path(save_folder, paste(prefix, group.by,
+        'dotplot.pdf', sep='_'))
+      p = DotPlot(tiss, genes_to_check, col.max = 2.5, plot.legend = T,
+        do.return = T, group.by = group.by) + coord_flip()
+      ggsave(filename, width = 3, height = 6)
+      dev.off()
+
+      filename = file.path(save_folder, paste(prefix, group.by,
+        'tsneplot.pdf', sep='_'))
+      p = TSNEPlot(object = tiss, do.return = TRUE, group.by = group.by)
+      ggsave(filename, width = 2, height = 2)
+      dev.off()
+
+      filename = file.path(save_folder, paste(prefix, group.by,
+        'violinplot.pdf', sep='_'))
+      p = ViolinPlot(tiss, genes_to_check, group.by = group.by, do.return=TRUE)
+      ggsave(filename, width = 6, height = 3)
+      dev.off()
+    }
+}
