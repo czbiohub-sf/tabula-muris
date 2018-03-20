3acad992cd1013303b8b5861358dfef4f6976e47
olgabot <olga.botvinnik@gmail.com>
2018-03-19 18:21:58 -0700

    Add reflow yaml code

---
+++ 28_tissue_yamls_for_supplement/reflow_yaml.py
@@ -0,0 +1,35 @@
+import click
+import yaml
+
+
+def listify(genes):
+    return genes.replace("'", '').replace(' ', '').split(',')
+
+@click.command()
+@click.argument('yamls', nargs=-1)
+def cli(yamls):
+    for filename in yamls:
+
+        # Contingency plan for now
+        if filename == 'brain_non-myeloid_facs.yaml':
+            continue
+        with open(filename) as f:
+            data = yaml.load(f)
+
+        data['GENES'] = listify(data["GENES"])
+        if 'SUBSET' in data:
+            for name, subset in data['SUBSET'].items():
+                subset['GENES'] = listify(subset['GENES'])
+
+        reflowed_data = yaml.dump(data,
+                                  # Add the --- at the beginning of the file
+                                  explicit_start=True,
+                                  # Turn flow style to false to get
+                                  # "bullet point" arrays
+                                  default_flow_style=False)
+        with open(filename + '.reflowed', 'w') as g:
+            g.write(reflowed_data)
+
+
+if __name__ == "__main__":
+    cli()
