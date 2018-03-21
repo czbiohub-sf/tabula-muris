2f9a28a81cde61e671ed9df10b6bddaff8c5008c
olgabot <olga.botvinnik@gmail.com>
2018-03-21 13:29:42 -0700

    Use pandas to make combined annotation file, separate out diaphragm and aorta

--- 00_data_ingest/18_global_annotation_csv/concat_all_annotations_by_method.py
+++ 00_data_ingest/18_global_annotation_csv/concat_all_annotations_by_method.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3.6
+
 import argparse
 import csv
 import glob
 import os.path
 
+import pandas as pd
+import numpy as np
 
 METHODS = ("facs", "droplet")
 INPUT_ANNOTATIONS_DIR = "../03_tissue_annotation_csv"
@@ -20,38 +24,32 @@ def get_header(csv_filename):
     return header
 
 
+subtissues = "Aorta", "Diaphragm"
+
 def main():
     args = parse_args()
+    method = args.method
 
-    all_annotations_csv_name = f"annotations_{args.method}.csv"
+    all_annotations_csv_name = f"annotations_{method}.csv"
     if os.path.isfile(all_annotations_csv_name):
         raise RuntimeError(f"{all_annotations_csv_name} already exists. Please remove before running.")
-    input_annotations = glob.glob(f"{INPUT_ANNOTATIONS_DIR}/*_{args.method.lower()}_annotation.csv")
+    input_annotations = glob.iglob(f"{INPUT_ANNOTATIONS_DIR}/*_{method.lower()}_annotation.csv")
 
-    # Get the header from one of the annotation files so we don't have to
-    # worry about if we have processed the header or not in the loop.
-    # This will also allow us to verify all of the headers are the same.
-    first_header_seen = get_header(input_annotations[0])
+    dfs = []
+    for annotation_file in input_annotations:
+        df = pd.read_csv(annotation_file)
 
-    with open(all_annotations_csv_name, 'w') as all_annotations_csv_file:
-        all_annotations_csv = csv.writer(all_annotations_csv_file,
-                                         delimiter=',', quoting=csv.QUOTE_ALL,
-                                         quotechar='"')
-        all_annotations_csv.writerow(first_header_seen)
+        # These subtissues were separated out for analysis and should just be
+        # called the "tissue"
+        if (method == 'facs') and (df['subtissue'][0] in subtissues):
+            df['tissue'] = df['subtissue']
+            df['subtissue'] = np.nan
 
-        for annotation_file in input_annotations:
-            with open(annotation_file) as csv_file:
-                annotation_data = csv.reader(csv_file, delimiter=',')
-                annotation_header = next(annotation_data)
-                if annotation_header != first_header_seen:
-                    os.remove(all_annotations_csv_name)
-                    raise RuntimeError(
-                        f"""The annotation file: {annotation_file} does not match the first header seen: {first_header_seen}.
-                           Failed to create {all_annotations_csv_name}.
-                        """
-                    )
-                for row in annotation_data:
-                    all_annotations_csv.writerow(row)
+        dfs.append(df)
+    all_annotations = pd.concat(dfs)
+
+    # index=False means don't write row numbers in the file
+    all_annotations.to_csv(all_annotations_csv_name, index=False)
 
 
 if __name__ == "__main__":
