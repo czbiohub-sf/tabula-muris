c8a789e7e85c9746bb5ce70b1e85238935755ff5
Olga Botvinnik <olga.botvinnik@gmail.com>
2018-03-23 20:20:26 -0700

    Use remote generate_from_template.py

--- utilities/generate_from_template.py
+++ utilities/generate_from_template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
+
 # coding: utf-8
 
 # This reads in a parameters file and generates an Rmd notebook
@@ -11,7 +12,6 @@ import sys
 # locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 # locale.setlocale(locale.LC_CTYPE, 'en_US.utf8')
 import os
-import re
 
 import yaml
 import click
@@ -28,37 +28,10 @@ GENES = "GENES"
 GROUPBY = "GROUPBY"
 CODE_FOLDER = '29_tissue-specific_supplement_code'
 
-DEFAULTS = {'res': 0.5, 'npcs': 20, 'genes': ['Actb'], 'groupby': None}
 
-
-def stringify_list(genes):
-    return ', '.join(map(lambda x: f'"{x}"', genes))
-
-
-def clean_name(name):
-    """Make the name nice looking for plots"""
-    if name.startswith('SUBSET'):
-        name = 'Subset' + name[-1]
-    else:
-        name = name.capitalize()
-    return name
-
-
-def add_subset(name, method, filter_column, filter_value, res, npcs, genes,
-               groupby):
-    """Add R code blocks for subsetting and reclustering"""
-    name = clean_name(name)
-
-    # If there are no digits in the label, then this is a string so stringify
-    try:
-        if len(re.findall('\d', filter_value)) == 0:
-            filter_value = f'"{filter_value}'
-    except TypeError:
-        # This is an integer, no modification needed
-        pass
-
-    filter = f'rownames(tiss@meta.data)[tiss@meta.data${filter_column} == {filter_value}]'
-    genes_str = stringify_list(genes)
+def add_subset(name, filter_column, filter_value, res, npcs, genes, groupby):
+    filter = f'rownames(tiss@meta.data)[grep("{filter_value}",tiss@meta.data${filter_column})]'
+    genes_str = ', '.join(map(lambda x: f'"{x}"', genes))
     code = f"""{name}.cells.use = {filter}
 {name}.n.pcs = {npcs}
 {name}.res = {res}
@@ -73,38 +46,18 @@ def add_subset(name, method, filter_column, filter_value, res, npcs, genes,
 """
     if groupby is not None:
         # Append this subset's groupby to the list
-        code += f'''\n# Append this subset's groupby to the list
-group.bys = c(group.bys, {stringify_list([groupby])})
-'''
-    code += f'''# Highlight which cells are in this subset
-    
-palette = brewer.pal(3, "YlGnBu")
-cols.use = c(palette[1], palette[3])
-colors.use
-tiss@meta.data[, "{name}"] = NA
-tiss@meta.data[{name}.cells.use, "{name}"] = "{name}" 
-filename = make_filename(save_folder, prefix="{name}", group.by, 
-    'tsneplot_allcells_highlighted')
-p = TSNEPlot(
-  object = tiss,
-  do.return = TRUE,
-  group.by = {name},
-  no.axes = TRUE,
-  pt.size = 1,
-  no.legend = TRUE,
-  colors.use = c(palette[1], 'grey50')
-)
-ggsave(filename, width = 4, height = 4)
-'''
+        code += '''\n# Append this subset's groupby to the list
+group.bys = c(group.bys, "{groupby}")'''
 
-    code += f'''dot_tsne_ridge({name}.tiss, {name}.genes_to_check,
-    save_folder, prefix = "{name}", group.bys, {method})
+    code += '''dot_tsne_violin({name}.tiss, {name}.genes_to_check,
+    save_folder, prefix = {name}, group.bys)
 '''
 
     codeblock = f'''```{{r}}
 {code}
 ```'''
     return codeblock
+
 
 
 
@@ -119,15 +72,13 @@ def main(parameters_yaml, template_file='Template.Rmd',
     with open(parameters_yaml) as f:
         parameters = yaml.load(f)
 
-    method = parameters[METHOD]
-
     with open(template_file) as f:
         template = f.read()
         for parameter, value in parameters.items():
             if parameter == ADDITIONAL_CODE:
                 # If ADDITIONAL_CODE is set to True
                 if value:
-                    basename = parameters[TISSUE] + "_" + method + '.Rmd'
+                    basename = parameters[TISSUE] + "_" + parameters[METHOD] + '.Rmd'
                     filename = os.path.join(
                         '..', CODE_FOLDER, basename)
                     with open(filename) as g:
@@ -137,16 +88,10 @@ def main(parameters_yaml, template_file='Template.Rmd',
                 for name, kv in value.items():
                     # Make all the keys have lowercase names
                     kv = {k.lower(): v for k, v in kv.items()}
-                    # Set defaults if they're not already
-                    for k, v in DEFAULTS.items():
-                        kv.setdefault(k, v)
-                    subset_code = add_subset(name, method, **kv)
+                    subset_code = add_subset(name, **kv)
+                    import pdb; pdb.set_trace()
                     template += f'\n## Subset: {name}\n\n{subset_code}'
             elif value is not None:
-                if parameter == GENES:
-                    value = stringify_list(value)
-                if parameter == GROUPBY:
-                    value = stringify_list([value])
                 template = template.replace("{" + parameter + "}", str(value))
             else:
                 template = template.replace("{" + parameter + "}", '')
@@ -155,7 +100,7 @@ def main(parameters_yaml, template_file='Template.Rmd',
             template = template.replace('{' + ADDITIONAL_CODE + '}',
                                         '# No additional code')
 
-    outfile = parameters[TISSUE] + "_" + method + suffix
+    outfile = parameters[TISSUE] + "_" + parameters[METHOD] + suffix
     with open(outfile, 'w') as f:
         f.write(template)
 
