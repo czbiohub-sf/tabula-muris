4009f10c30d254af9eebf1e02356a9978f29f8cc
olgabot <olga.botvinnik@gmail.com>
2018-03-28 13:27:41 -0700

    Turn tSNE and ridgeplots into functsoin

--- 30_tissue_supplement_figures/supplemental_figures.R
+++ 30_tissue_supplement_figures/supplemental_figures.R
@@ -87,6 +87,140 @@ get_valid_genes = function(tiss, genes_to_check){
   return(genes_to_check)
 }
 
+ridgeplot_and_save = function(tiss, save_folder, prefix, group.by, genes,
+  colors.use=NULL, i=0, n_chunks=0, suffix=''){
+        if (colors.use == NULL){
+        colors.use = get_colors(tiss, group.by)
+    }
+
+
+  if (length(suffix) == 0){
+    suffix = paste0(i, '-of-', n_chunks)
+  }
+        filename = make_filename(save_folder, prefix, group.by,
+                               paste('ridgeplot', suffix, sep='_'))
+        # Adjust canvas size so the plots are the same size no matter how many genes
+      nCol = 4
+      nRow = ceiling(length(genes) / nCol)
+
+      # Set height of ridgeplots
+      ridge_height = 5.5
+
+      plots = RidgePlot(
+        tiss,
+        genes,
+        group.by = group.by,
+        do.return = TRUE,
+        nCol = 4,
+        same.y.lims = TRUE,
+        return.plotlist = TRUE,
+        cols.use = colors.use
+      )
+      for (i in seq(1, length(plots))) {
+        p = plots[[i]]
+        p = p + xlab(expression_unit) + scale_x_continuous(breaks = pretty_breaks(n = 4)) + scale_y_discrete(limits = rev(levels(group.by)))
+        p = add_black_na(p, group.by, colors.use, plottype='ridge')
+        plots[[i]] = p
+      }
+      plots.combined <- plot_grid(plotlist = plots, ncol = nCol)
+      invisible(x = lapply(X = plots.combined, FUN = print))
+      ggsave(filename,
+             width = 16,
+             height = nRow * ridge_height)
+  return(filename)
+}
+
+tsne_and_legend = function(tiss,
+                           save_folder,
+                           prefix,
+                           group.by, colors.use=NULL,  do.label = FALSE){
+
+    if (colors.use == NULL){
+        colors.use = get_colors(tiss, group.by)
+    }
+      # TSNE only
+    tsne_filename = make_filename(save_folder, prefix, group.by, 'tsneplot')
+    p = TSNEPlot(
+      object = tiss,
+      do.return = TRUE,
+      group.by = group.by,
+      no.axes = TRUE,
+      pt.size = 1,
+      no.legend = TRUE,
+      colors.use = colors.use,
+      do.label=do.label
+    )
+    p = add_black_na(p, group.by, colors.use, plottype='tsne')
+    ggsave(tsne_filename, width = 4, height = 4)
+    # dev.off()
+
+    # Legend for TSNE
+    # Add letter to the string labels groupby for legends
+    if (group.by != 'cluster.ids'){
+      labels = sort(unique(tiss@meta.data[, group.by]))
+      letters = LETTERS[seq(1, length(labels))]
+      new_labels = paste0('(', letters, ') ', labels)
+      group.by.plus = paste0(group.by, '_letter')
+      tiss@meta.data[, group.by.plus] = plyr::mapvalues(
+        x = tiss@meta.data[, group.by],
+        from = labels,
+        to = new_labels
+      )
+    } else {
+      # Make sure cluster ids are numeric so they're sorted properly
+      tiss@meta.data[, group.by] = as.numeric(tiss@meta.data[, group.by])
+      group.by.plus = group.by
+    }
+
+    legend_filename = make_filename(save_folder, prefix, group.by, 'tsneplot_legend')
+    # Plot TSNE again just to steal the legend
+    p = TSNEPlot(
+      object = tiss,
+      do.return = TRUE,
+      group.by = group.by.plus,
+      no.axes = TRUE,
+      pt.size = 1,
+      no.legend = FALSE,
+      label.size = 8,
+      colors.use = colors.use
+    ) + coord_fixed(ratio = 1) +
+      xlab("tSNE 1") + ylab("tSNE 2")
+    p = add_black_na(p, group.by, colors.use, plottype='tsne')
+
+    # Initialize an empty canvas!
+    ggdraw()
+    # Draw only the legend
+    ggdraw(g_legend(p))
+    ggsave(legend_filename, width = 8, height = 4)
+    dev.off()
+  return(tsne_filename)
+}
+
+get_colors = function(tiss, group.by){
+    annotations = sort(unique(tiss@meta.data[,group.by]))
+    n_annotations = length(annotations)
+
+        # Get number of colors to use
+    if (group.by == 'cell_ontology_class') {
+      if (n_annotations > 8) {
+        if (n_annotations > 16){
+          colors.use = c(brewer.pal(8, 'Set2'), brewer.pal(8, 'Dark2'),
+            brewer.pal(max(n_annotations -16, 3), 'Pastel2'))
+
+        } else {
+          colors.use = c(brewer.pal(8, 'Set2'), brewer.pal(max(n_annotations - 8, 3), 'Dark2'))
+        }
+      } else{
+        colors.use = brewer.pal(max(n_annotations, 3), 'Set2')
+      }
+      # Add black for NA
+      # colors.use = c(colors.use, 'black')
+    } else {
+      colors.use = NULL
+    }
+    return (colors.use)
+}
+
 # Make all supplemental figures for a tiss object
 dot_tsne_ridge = function(tiss,
                            genes_to_check,
@@ -135,62 +269,9 @@ dot_tsne_ridge = function(tiss,
     } else {
       colors.use = NULL
     }
-    
-    # TSNE only
-    filename = make_filename(save_folder, prefix, group.by, 'tsneplot')
-    p = TSNEPlot(
-      object = tiss,
-      do.return = TRUE,
-      group.by = group.by,
-      no.axes = TRUE,
-      pt.size = 1,
-      no.legend = TRUE,
-      colors.use = colors.use
-    )
-    p = add_black_na(p, group.by, colors.use, plottype='tsne')
-    ggsave(filename, width = 4, height = 4)
-    # dev.off()
-    
-    # Legend for TSNE
-    # Add letter to the string labels groupby for legends
-    if (group.by != 'cluster.ids'){
-      labels = sort(unique(tiss@meta.data[, group.by]))
-      letters = LETTERS[seq(1, length(labels))]
-      new_labels = paste0('(', letters, ') ', labels)
-      group.by.plus = paste0(group.by, '_letter')
-      tiss@meta.data[, group.by.plus] = plyr::mapvalues(
-        x = tiss@meta.data[, group.by],
-        from = labels,
-        to = new_labels
-      )
-    } else {
-      # Make sure cluster ids are numeric so they're sorted properly
-      tiss@meta.data[, group.by] = as.numeric(tiss@meta.data[, group.by])
-      group.by.plus = group.by
-    }
 
-    filename = make_filename(save_folder, prefix, group.by, 'tsneplot_legend')
-    # Plot TSNE again just to steal the legend
-    p = TSNEPlot(
-      object = tiss,
-      do.return = TRUE,
-      group.by = group.by.plus,
-      no.axes = TRUE,
-      pt.size = 1,
-      no.legend = FALSE,
-      label.size = 8,
-      colors.use = colors.use
-    ) + coord_fixed(ratio = 1) +
-      xlab("tSNE 1") + ylab("tSNE 2")
-    p = add_black_na(p, group.by, colors.use, plottype='tsne')
-    
-    # Initialize an empty canvas!
-    ggdraw()
-    # Draw only the legend
-    ggdraw(g_legend(p))
-    ggsave(filename, width = 8, height = 4)
-    dev.off()
-    
+    tsne_and_legend(tiss, save_folder, prefix, group.by, colors.use)
+
     # Many of the strings are too long so we use numbers instead. The color
     # scheme is the same as the TSNE as long as we sort alphabetically.
     # Make a temporary copy of the column to store the original strings
@@ -200,29 +281,24 @@ dot_tsne_ridge = function(tiss,
       from = labels,
       to = letters
     )
-    
-    # Reverse the order of the identities to make it easier to read
-    ident.use = rev(sort(unique(tiss@meta.data[, group.by])))
+
     
     # Break up the gene list into reasonable lists for legible pages
     chunked_genes = split(genes_to_check, ceiling(seq_along(genes_to_check) /
                                                     CHUNKSIZE))
     n_chunks = length(chunked_genes)
-    for (name in names(chunked_genes)) {
-      write(paste('\tchunk:', name), stderr())
+    for (i in names(chunked_genes)) {
+      write(paste('\tchunk:', i), stderr())
       
-      genes = chunked_genes[[name]]
-      
-      # Adjust canvas size so the plots are the same size no matter how many genes
-      nCol = 4
-      nRow = ceiling(length(genes) / nCol)
+      genes = chunked_genes[[i]]
+
       
       # Set height of ridgeplots
       ridge_height = 5.5
       
       # Dotplot - enrichment of gene expression in group.by with dot size
         filename = make_filename(save_folder, prefix, group.by,
-          paste0('dotplot_', name, '-of-', n_chunks))
+          paste0('dotplot_', i, '-of-', n_chunks))
       # Use rev(genes) to reverse the order because dotplot does Right to Left instead of left to right
       p = DotPlot(
         tiss,
@@ -240,30 +316,7 @@ dot_tsne_ridge = function(tiss,
       ggsave(filename, width = 13.75, height = 10)
       # dev.off()
       
-      # Ridgeplot - enrichment of gene expression in group.by with smoothed histograms
-      filename = make_filename(save_folder, prefix, group.by,
-                               paste0('ridgeplot_', name, '-of-', n_chunks))
-      plots = RidgePlot(
-        tiss,
-        genes,
-        group.by = group.by,
-        do.return = TRUE,
-        nCol = 4,
-        same.y.lims = TRUE,
-        return.plotlist = TRUE,
-        cols.use = colors.use
-      )
-      for (i in seq(1, length(plots))) {
-        p = plots[[i]]
-        p = p + xlab(expression_unit) + scale_x_continuous(breaks = pretty_breaks(n = 4)) + scale_y_discrete(limits = rev(levels(group.by)))
-        p = add_black_na(p, group.by, colors.use, plottype='ridge')
-        plots[[i]] = p
-      }
-      plots.combined <- plot_grid(plotlist = plots, ncol = nCol)
-      invisible(x = lapply(X = plots.combined, FUN = print))
-      ggsave(filename,
-             width = 16,
-             height = nRow * ridge_height)
+      ridgeplot_and_save(tiss, save_folder, prefix, group.by, genes, colors.use, i, n_chunks)
       # dev.off()
     }
     
