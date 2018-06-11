0490ac481e52be20dadb6b4154846a3f1940b6eb
Joshua Batson <joshua.batson@czbiohub.org>
2018-06-11 12:20:07 -0700

    extract tsne coordinates from global R objects

---
+++ 00_data_ingest/18_global_annotation_csv/extract_tsne_coords.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(stringr)
+library(Seurat)
+library(here)
+
+load(file=here("00_data_ingest", "11_global_robj", "FACS_all.Robj"))
+
+df = FetchData(tiss_FACS, c('tSNE_1', 'tSNE_2', 'cluster'))
+write_csv(df, here("00_data_ingest", "18_global_annotation_csv", "tsne_facs.csv"))
+
+load(file=here("00_data_ingest", "11_global_robj", "droplet_all.Robj"))
+
+df_droplet = FetchData(tiss_droplet, c('tSNE_1', 'tSNE_2', 'cluster'))
+write_csv(df_droplet, here("00_data_ingest", "18_global_annotation_csv", "tsne_droplet.csv"))
